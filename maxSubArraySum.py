05165a40cf879bb4b321d3d5dbf5247a5fbb795c
Sushma <tmblweed@users.noreply.github.com>
2017-05-19 16:29:12 -0700

    Create maxSubArraySum.py

---
+++ maxSubArraySum.py
@@ -0,0 +1,18 @@
+def maxSubArraySum(a,size):
+     
+    max_so_far = 0
+    max_ending_here = 0
+     
+    for i in range(0, size):
+        max_ending_here = max_ending_here + a[i]
+        if max_ending_here < 0:
+            max_ending_here = 0
+         
+        if (max_so_far < max_ending_here):
+            max_so_far = max_ending_here
+             
+    return max_so_far
+
+a = [-2, -3, -4, -1, -2, -1, -5, 120,300]
+
+print maxSubArraySum(a,len(a))
