f80c446659838b93d9271bbdc3bc822bfe35922b
Sushma <tmblweed@users.noreply.github.com>
2017-05-30 09:22:32 -0700

    Create merge.py

---
+++ merge.py
@@ -0,0 +1,26 @@
+#merginig part of merge sort
+
+def merge(left, right):
+	if not len(left) or not len(right):
+		return left or right
+
+	result = []
+	i, j = 0, 0
+	while (len(result) < len(left) + len(right)):
+		if left[i] < right[j]:
+			result.append(left[i])
+			i+= 1
+		else:
+			result.append(right[j])
+			j+= 1
+		if i == len(left) or j == len(right):
+			result.extend(left[i:] or right[j:])
+			break 
+
+
+	return result
+
+A = [2,5,7]
+B = [1,3,8]
+
+print merge(A,B)
