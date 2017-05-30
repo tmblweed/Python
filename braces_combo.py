0b05fb1e0f95b1abfe77fa4bb05d0d07b6157053
Sushma <tmblweed@users.noreply.github.com>
2017-05-30 09:21:20 -0700

    Create braces_combo.py

---
+++ braces_combo.py
@@ -0,0 +1,11 @@
+def braces_combinations(output, open_brace, close_brace, pairs):
+"""for each combination, you cannot have more open braces than number of pairs or more closed than open"""
+    if open_brace == pairs and close_brace == pairs:
+        print output
+    else:
+        if open_brace<pairs:
+            braces_combinations(output+'(', open_brace+1, close_brace, pairs)
+        if close_brace<close_brace: #you want every closed brace to have an open brace
+            braces_combinations(output+')', open_brace, close_brace+1, pairs)
+
+braces_combinations('', 0, 0, 3)
